f74c1db8df3215db22036e1dba8347fd0ec3d305
Liam Howard <lima.mail725@gmail.com>
2024-12-07 18:44:55 +1000

    Updated to use templates.

--- include/utils/color.hpp
+++ include/utils/color.hpp
@@ -12,32 +12,22 @@ namespace muon {
             *
             *  Expected integer structure: RR_GG_BB_AA
         */
-        std::array<float, 4> hexToRgbaArray(uint32_t color);
-        std::array<float, 4> hexBytesToRgbaArray(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+        template <typename T>
+        T hexToRgba(uint32_t color);
 
-        /**
-            *  Four byte hex to RGBA
-            *
-            *  Expected integer structure: RR_GG_BB_AA
-        */
-        glm::vec4 hexToRgbaVec(uint32_t color);
-        glm::vec4 hexBytesToRgbaVec(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+        template <typename T>
+        T hexBytesToRgba(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
 
         /**
             *  Three byte hex to RGB
             *
             *  Expected integer structure: XX_RR_GG_BB
         */
-        std::array<float, 3> hexToRgbArray(uint32_t color);
-        std::array<float, 3> hexBytesToRgbArray(uint8_t r, uint8_t g, uint8_t b);
+        template <typename T>
+        T hexToRgb(uint32_t color);
 
-        /**
-            *  Three byte hex to RGB
-            *
-            *  Expected integer structure: XX_RR_GG_BB
-        */
-        glm::vec3 hexToRgbVec(uint32_t color);
-        glm::vec3 hexBytesToRgbVec(uint8_t r, uint8_t g, uint8_t b);
+        template <typename T>
+        T hexBytesToRgb(uint8_t r, uint8_t g, uint8_t b);
     }
 
 }
--- src/utils/color.cpp
+++ src/utils/color.cpp
@@ -1,76 +1,80 @@
 #include "utils/color.hpp"
 
-#include <climits>
-
 namespace muon {
     namespace color {
-        std::array<float, 4> hexToRgbaArray(uint32_t color) {
+        template <>
+        std::array<float, 4> hexToRgba<std::array<float, 4>>(uint32_t color) {
             return {
-                static_cast<float>((color >> 0x18) & 0xFF) / UCHAR_MAX,
-                static_cast<float>((color >> 0x10) & 0xFF) / UCHAR_MAX,
-                static_cast<float>((color >> 0x08) & 0xFF) / UCHAR_MAX,
-                static_cast<float>((color >> 0x00) & 0xFF) / UCHAR_MAX
+                static_cast<float>((color >> 0x18) & 0xFF) / 0xFF,
+                static_cast<float>((color >> 0x10) & 0xFF) / 0xFF,
+                static_cast<float>((color >> 0x08) & 0xFF) / 0xFF,
+                static_cast<float>((color >> 0x00) & 0xFF) / 0xFF
             };
         }
 
-        std::array<float, 4> hexBytesToRgbaArray(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
+        template <>
+        glm::vec4 hexToRgba<glm::vec4>(uint32_t color) {
             return {
-                static_cast<float>(r) / UCHAR_MAX,
-                static_cast<float>(g) / UCHAR_MAX,
-                static_cast<float>(b) / UCHAR_MAX,
-                static_cast<float>(a) / UCHAR_MAX
+                static_cast<float>((color >> 0x18) & 0xFF) / 0xFF,
+                static_cast<float>((color >> 0x10) & 0xFF) / 0xFF,
+                static_cast<float>((color >> 0x08) & 0xFF) / 0xFF,
+                static_cast<float>((color >> 0x00) & 0xFF) / 0xFF
             };
         }
 
-        glm::vec4 hexToRgbaVec(uint32_t color) {
+        template <>
+        std::array<float, 4> hexBytesToRgba<std::array<float, 4>>(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
             return {
-                static_cast<float>((color >> 0x18) & 0xFF) / UCHAR_MAX,
-                static_cast<float>((color >> 0x10) & 0xFF) / UCHAR_MAX,
-                static_cast<float>((color >> 0x08) & 0xFF) / UCHAR_MAX,
-                static_cast<float>((color >> 0x00) & 0xFF) / UCHAR_MAX
+                static_cast<float>(r) / 0xFF,
+                static_cast<float>(g) / 0xFF,
+                static_cast<float>(b) / 0xFF,
+                static_cast<float>(a) / 0xFF
             };
         }
 
-        glm::vec4 hexBytesToRgbaVec(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
+        template <>
+        glm::vec4 hexBytesToRgba<glm::vec4>(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
             return {
-                static_cast<float>(r) / UCHAR_MAX,
-                static_cast<float>(g) / UCHAR_MAX,
-                static_cast<float>(b) / UCHAR_MAX,
-                static_cast<float>(a) / UCHAR_MAX
+                static_cast<float>(r) / 0xFF,
+                static_cast<float>(g) / 0xFF,
+                static_cast<float>(b) / 0xFF,
+                static_cast<float>(a) / 0xFF
             };
         }
 
-        std::array<float, 3> hexToRgbArray(uint32_t color) {
-            color &= 0xFFFFFF;
+        template <>
+        std::array<float, 3> hexToRgb<std::array<float, 3>>(uint32_t color) {
             return {
-                static_cast<float>((color >> 0x10) & 0xFF) / UCHAR_MAX,
-                static_cast<float>((color >> 0x08) & 0xFF) / UCHAR_MAX,
-                static_cast<float>((color >> 0x00) & 0xFF) / UCHAR_MAX
+                static_cast<float>((color >> 0x10) & 0xFF) / 0xFF,
+                static_cast<float>((color >> 0x08) & 0xFF) / 0xFF,
+                static_cast<float>((color >> 0x00) & 0xFF) / 0xFF
             };
         }
 
-        std::array<float, 3> hexBytesToRgbArray(uint8_t r, uint8_t g, uint8_t b) {
+        template <>
+        glm::vec3 hexToRgb<glm::vec3>(uint32_t color) {
             return {
-                static_cast<float>(r) / UCHAR_MAX,
-                static_cast<float>(g) / UCHAR_MAX,
-                static_cast<float>(b) / UCHAR_MAX
+                static_cast<float>((color >> 0x10) & 0xFF) / 0xFF,
+                static_cast<float>((color >> 0x08) & 0xFF) / 0xFF,
+                static_cast<float>((color >> 0x00) & 0xFF) / 0xFF
             };
         }
 
-        glm::vec3 hexToRgbVec(uint32_t color) {
-            color &= 0xFFFFFF;
+        template <>
+        std::array<float, 3> hexBytesToRgba<std::array<float, 3>>(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
             return {
-                static_cast<float>((color >> 0x10) & 0xFF) / UCHAR_MAX,
-                static_cast<float>((color >> 0x08) & 0xFF) / UCHAR_MAX,
-                static_cast<float>((color >> 0x00) & 0xFF) / UCHAR_MAX
+                static_cast<float>(r) / 0xFF,
+                static_cast<float>(g) / 0xFF,
+                static_cast<float>(b) / 0xFF
             };
         }
 
-        glm::vec3 hexBytesToRgbVec(uint8_t r, uint8_t g, uint8_t b) {
+        template <>
+        glm::vec3 hexBytesToRgb<glm::vec3>(uint8_t r, uint8_t g, uint8_t b) {
             return {
-                static_cast<float>(r) / UCHAR_MAX,
-                static_cast<float>(g) / UCHAR_MAX,
-                static_cast<float>(b) / UCHAR_MAX
+                static_cast<float>(r) / 0xFF,
+                static_cast<float>(g) / 0xFF,
+                static_cast<float>(b) / 0xFF
             };
         }
     }
